9ab7f2681d866681f9be4fa40e3a5c75ccd13585
楊景翔 <zzz6992926gmail.com>
2024-06-04 05:58:26 +0800

    Let main() can run 10 different test funciton

--- config.hpp
+++ config.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include<unordered_map>
 #include<string>
 #include<vector>
@@ -9,12 +11,14 @@ const int BASE_EVALUATION = 10000;
 const vector<int> DIMENSION = {2, 10, 30};
 const int POPULATION_SIZE = 64;
 const int MAX_GROUP_SIZE = 8;
-const int LOCAL_LIMIT = 64;
-const int GLOBAL_LIMIT = 64;
-const double PERTUBATION_RATE = 0.6; 
+const int LOCAL_LIMIT = 32;
+const int GLOBAL_LIMIT = 32;
+const double PERTUBATION_RATE = 0.5;
+const double RANDOM_LOCATION_RATE = 0.8;
+const int RECORD_PER_EVALUATION = 200; 
 
 const vector<string> FUNCTION_LIST = {
-    "Ackley", "Rastrigin", "HappyCat", "Rosenbrock", "Zakharov", "Michalewicz"
+    "Ackley", "Rastrigin", "HappyCat", "Rosenbrock", "Zakharov", "Michalewicz", "Schwefel", "BentCigar", "DropWave", "Step"
 };
 unordered_map<string, int> TEST_FUNCTION= {
     {"Ackley", 1},
@@ -22,5 +26,9 @@ unordered_map<string, int> TEST_FUNCTION= {
     {"HappyCat", 3},
     {"Rosenbrock", 4},
     {"Zakharov", 5},
-    {"Michalewicz", 6}
+    {"Michalewicz", 6},
+    {"Schwefel", 7},
+    {"BentCigar", 8},
+    {"DropWave", 9},
+    {"Step", 10},
 };
--- main.cpp
+++ main.cpp
@@ -1,40 +1,54 @@
 #include <iostream>
 #include <fstream>
-#include <chrono> 
+#include <chrono>
+#include <map>
+#include <vector>
 #include "smo.hpp"
+#include "asmo.hpp"
 
 
+using namespace smo;
+using namespace asmo;
 using namespace std;
 using namespace std::chrono; 
 
 int main(){
 
-    std::ofstream outputFile[2];
-    string folder[2] = {"smo_original/", "smo_levy/"};
+    std::ofstream outputFile[3];
+    string folder[3] = {"smo_original/", "smo_levy/", "asmo/"};
 
     for(int i=0 ; i<FUNCTION_LIST.size() ; i++){
         for(int d=0 ; d<DIMENSION.size() ; d++){
             int dimensions = DIMENSION[d];
             int evalutions = dimensions * BASE_EVALUATION;
 
-            for(int k=0 ; k<2 ; k++){
+            for(int k=0 ; k<3 ; k++){
                 outputFile[k].open(folder[k] + FUNCTION_LIST[i]+ "d" + to_string(dimensions) + ".txt");
                 auto start = high_resolution_clock::now(); // 開始計時
+                
+                unordered_map<int, vector<double>> record;
 
                 for(int j=1 ; j<=RUNS ; j++){
                     switch (k)
                     {
                         // not use levy flight
                         case 0:{
-                            SMO smo(LOCAL_LIMIT, GLOBAL_LIMIT, POPULATION_SIZE, PERTUBATION_RATE);
-                            double result = smo.run(FUNCTION_LIST[i], dimensions, j, evalutions);
+                            smo::SMO smo(LOCAL_LIMIT, GLOBAL_LIMIT, POPULATION_SIZE, PERTUBATION_RATE, false);
+                            double result = smo.run(FUNCTION_LIST[i], dimensions, j, evalutions, record);
                             outputFile[k] << "Run" << j <<" Result: " << result << endl;
                             break;
                         }
                         // use levy flight
                         case 1:{
-                            SMO smo(LOCAL_LIMIT, GLOBAL_LIMIT, POPULATION_SIZE, PERTUBATION_RATE, true);
-                            double result  = smo.run(FUNCTION_LIST[i], dimensions, j, evalutions);
+                            smo::SMO smo(LOCAL_LIMIT, GLOBAL_LIMIT, POPULATION_SIZE, PERTUBATION_RATE, true);
+                            double result  = smo.run(FUNCTION_LIST[i], dimensions, j, evalutions, record);
+                            outputFile[k] << "Run" << j <<" Result: " << result << endl;
+                            break;
+                        }
+                        // use ASMO (without levy flight)
+                        case 2:{
+                            asmo::ASMO asmo(LOCAL_LIMIT, GLOBAL_LIMIT, POPULATION_SIZE, PERTUBATION_RATE, true);
+                            double result  = asmo.run(FUNCTION_LIST[i], dimensions, j, evalutions, record);
                             outputFile[k] << "Run" << j <<" Result: " << result << endl;
                             break;
                         }
@@ -42,6 +56,23 @@ int main(){
                             break;
                     }
                 }
+
+                
+                for(int e=0; e<record[1].size() ; e++){
+                    double acc = 0;
+                    for(int j=1 ; j<=RUNS ; j++){
+                        acc += record[j][e];
+                    }
+                    acc /= RUNS;
+                    outputFile[k] << "evalution : " << (e+1) * RECORD_PER_EVALUATION << " -----> " << acc << "  (optimal value)" << endl;
+                }
+                
+                /*
+                for(int v=0 ; v<record[1].size() ; v++)
+                    cout << "evalutions : " << (v+1) * RECORD_PER_EVALUATION << " -----> " << record[1][v] << endl;
+                */
+
+            
 
                 auto stop = high_resolution_clock::now(); // 結束計時
                 auto duration = duration_cast<milliseconds>(stop - start); // 計算執行時間
